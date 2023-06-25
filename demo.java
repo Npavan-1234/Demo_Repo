a7379b2342686de28923f7f95b2a0398ad0aaae7
npavan-1234 <npavankumar5757@gmail.com>
2023-06-25 13:56:15 +0530

    iogh

--- demo.java
+++ demo.java
@@ -1,9 +1,7 @@
-public class Demo{
- public static void main(String args[]){
-    println("hello");
-    
- }
+public class Demo {
+   public static void main(String args[]) {
+      System.out.println("hello");
 
-
+   }
 
 }
